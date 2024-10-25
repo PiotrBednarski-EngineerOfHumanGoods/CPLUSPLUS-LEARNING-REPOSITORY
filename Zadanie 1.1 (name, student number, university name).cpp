900d046cbd3368d2609b676bd13ba400690a0b95
PiotrBednarski-EngineerOfHumanGoods <piotr.bednarski.tech@gmail.com>
2024-10-25 21:57:47 +0200

    Add files via upload

---
+++ Zadanie 1.1 (name, student number, university name).cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Imię: Piotr" << endl;
+    cout << "Numer studenta: s33711" << endl;
+    cout << "Nazwa uczelni: Polsko-Japońska Akademia Technik Komputerowych Wydział Zamiejscowy w Gdańsku" << endl;
+    return 0;
+}
