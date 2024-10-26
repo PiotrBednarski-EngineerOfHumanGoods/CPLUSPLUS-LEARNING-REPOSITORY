5c483ab5abb164bb81dbc3aad0eb838ce340ae02
PiotrBednarski-EngineerOfHumanGoods <piotr.bednarski.tech@gmail.com>
2024-10-26 14:46:08 +0200

    Update Zadanie 2.0 (pizza party).cpp
    I corrected the code to inform that there are more guests than pieces of pizza.

--- Zadanie 2.0 (pizza party).cpp
+++ Zadanie 2.0 (pizza party).cpp
@@ -3,23 +3,25 @@ using namespace std;
 
 int main() {
     int ilośćgości, ilośćkawałków;
-    
-    cout <<"Podaj ile przyszło gości: ";
+
+    cout << "Podaj ile przyszło gości: ";
     cin >> ilośćgości;
-    cout <<"Podaj liczbę pokrojonych kawałków pizzy: ";
+    cout << "Podaj liczbę pokrojonych kawałków pizzy: ";
     cin >> ilośćkawałków;
-    
+
     if (ilośćgości > 0) {
-        int kawałkinagościa = ilośćkawałków / ilośćgości;
-        int resztadlaorganizatora = ilośćkawałków % ilośćgości;
-        
-        cout << "Każdy gość otrzyma " << kawałkinagościa << " kawałków pizzy." << endl;
-        cout << "Pozostałe kawałki dla organizatora to: " << resztadlaorganizatora << endl;
-        
+        if (ilośćkawałków < ilośćgości) {
+            cout << "Za mało kawałków pizzy dla gości!" << endl;
+        } else {
+            int kawałkinagościa = ilośćkawałków / ilośćgości;
+            int resztadlaorganizatora = ilośćkawałków % ilośćgości;
+
+            cout << "Każdy gość otrzyma " << kawałkinagościa << " kawałków pizzy." << endl;
+            cout << "Pozostałe kawałki dla organizatora to: " << resztadlaorganizatora << endl;
+        }
     } else {
-        cout <<"Liczba gości nie może być równa 0." << endl;
-        
+        cout << "Liczba gości nie może być równa 0." << endl;
     }
-    
+
     return 0;
 }
