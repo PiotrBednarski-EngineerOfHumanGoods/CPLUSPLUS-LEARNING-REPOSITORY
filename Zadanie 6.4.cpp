23bbb9c3d409f9acadd84bfa0f77dafd80a87756
PiotrBednarski-EngineerOfHumanGoods <piotr.bednarski.tech@gmail.com>
2025-01-17 23:38:25 +0100

    Add files via upload
    A program that concatenate a string provided by the user n times. The number n should also be entered by the user.

---
+++ Zadanie 6.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string tekst, wynik = "";
+    int n;
+
+    cout << "Podaj tekst: ";
+    cin >> tekst;
+
+    cout << "Podaj liczbę powtorzen: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        wynik += tekst;
+    }
+
+    cout << "Wynikowy tekst: " << wynik << endl;
+
+    return 0;
+}
