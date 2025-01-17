64cdebe86d68636a47be528d06e5039f44a12edf
PiotrBednarski-EngineerOfHumanGoods <piotr.bednarski.tech@gmail.com>
2025-01-17 23:31:37 +0100

    Add files via upload
    A program to sort a vector of student structures by index numbers. The Student structure should contain information about the following: first name, last name, and index number.

---
+++ Zadanie 6.3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct Student {
+    string imie;
+    string nazwisko;
+    int indeks;
+};
+
+bool porownajIndeksy(const Student& a, const Student& b) {
+    return a.indeks < b.indeks;
+}
+
+int main() {
+    vector<Student> studenci = {
+        {"Leo", "Kowal", 72165},
+        {"Maciej", "Demp", 783},
+        {"Alicja", "Kolecka", 260000},
+        {"Katarzyna", "Szczaw", 15}
+    };
+
+    cout << "Wektor przed sortowaniem:\n";
+    for (const auto& student : studenci) {
+        cout << student.indeks << " " << student.imie << " " << student.nazwisko << "\n";
+    }
+
+    sort(studenci.begin(), studenci.end(), porownajIndeksy);
+
+    cout << "\nWektor posortowany:\n";
+    for (const auto& student : studenci) {
+        cout << student.indeks << " " << student.imie << " " << student.nazwisko << "\n";
+    }
+
+    return 0;
+}
