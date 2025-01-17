47c83ffcb1d0561d98c612c10c95b5ea003a4cf3
PiotrBednarski-EngineerOfHumanGoods <piotr.bednarski.tech@gmail.com>
2025-01-17 21:38:03 +0100

    Add files via upload
    A program that calculates how many natural numbers (i.e., integers greater than zero) not exceeding 𝑛 are divisible by 5 but not divisible by 3. The number 𝑛 should be provided by the user.

---
+++ Zadanie 6.1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int policz_liczby(int n);
+
+
+int main() {
+    int n;
+    cout << "Podaj liczbe n: ";
+
+
+    if (cin >> n) {
+        if (n < 1) {
+            cout << "Liczba musi byc dodatnia!" << endl;
+        }
+        else {
+            int wynik = policz_liczby(n);
+            cout << "Liczb spelniajacych warunki (liczby podzielne przez 5, ale nie podzielne przez 3) jest: " << wynik << endl;
+        }
+    }
+    else {
+        cout << "Podaj poprawna liczbe calkowita!" << endl;
+    }
+
+    return 0;
+}
+
+
+int policz_liczby(int n) {
+    if (n < 1) {
+        return 0;
+    }
+
+    int licznik = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 5 == 0 && i % 3 != 0) {
+            licznik++;
+        }
+    }
+    return licznik;
+}
