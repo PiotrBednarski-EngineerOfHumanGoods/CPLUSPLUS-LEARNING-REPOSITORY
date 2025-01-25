a0939c010fb94893f6490062e7829028d333d0c2
PiotrBednarski-EngineerOfHumanGoods <piotr.bednarski.tech@gmail.com>
2025-01-25 02:01:49 +0100

    Add files via upload

---
+++ Zadanie 7.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double promien, pole;
+
+    cout << "Podaj promien kola: ";
+    cin >> promien;
+
+    cout << "Pole kola wynosi: ";
+    pole = 3.14159 * promien * promien;
+    cout << fixed << setprecision(3) << pole << endl;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << setw(8) << pole << " ";
+            pole += 3;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
